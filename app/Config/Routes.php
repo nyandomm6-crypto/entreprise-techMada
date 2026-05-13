71119b71e043b600ff56177d6f90cd5dfa3ee709
Miora <durocmiora@gmail.com>
2026-05-13 11:22:06 +0300

    lo

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,17 +12,17 @@ $routes->post('/login', 'front\EmployesController::loginPost');
 $routes->get('/logout', 'front\EmployesController::logout');
 
 // emlpoye
-$routes->group('', ['filter' => 'employe'], function($routes) {
-    $routes->get('/employes/dashboard', 'employes\DashboardController::index');
-});
+// $routes->group('', ['filter' => 'employe'], function($routes) {
+//     $routes->get('/employes/dashboard', 'employes\DashboardController::index');
+// });
 
 
-// rh
-$routes->group('', ['filter' => 'rh'], function($routes) {
-    $routes->get('/rh/dashboard', 'rh\DashboardController::index');
-});
+// // rh
+// $routes->group('', ['filter' => 'rh'], function($routes) {
+//     $routes->get('/rh/dashboard', 'rh\DashboardController::index');
+// });
 
-// admin
-$routes->group('', ['filter' => 'admin'], function($routes) {
-    $routes->get('/admin/dashboard', 'admin\DashboardController::index');
-});
+// // admin
+// $routes->group('', ['filter' => 'admin'], function($routes) {
+//     $routes->get('/admin/dashboard', 'admin\DashboardController::index');
+// });
